fa396165e65230c0f080118bc6cf8f375d81a4c8
aymeric s <sandoz.aymeric@gmail.com>
2022-11-08 21:49:53 +0100

    config

--- front/app.js
+++ front/app.js
@@ -46,83 +46,83 @@ setTimeout(function () {
 
 ///////////////////////
 
-// const fetchOwnerCardList = () => {
-//   fetch("api/card/ownerCards", {
-//     method: "GET",
-//     headers: {
-//       authorization: `Bearer ${localStorage.getItem("token")}`,
-//     },
-//   })
-//     .then((reponse) => reponse.json())
-//     .then((allSpikemmon) => {
-//       console.log(allSpikemmon);
-//       createCard(allSpikemmon);
-//       chargement.style.display = "none";
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
+const fetchOwnerCardList = () => {
+  fetch("api/card/ownerCards", {
+    method: "GET",
+    headers: {
+      authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  })
+    .then((reponse) => reponse.json())
+    .then((allSpikemmon) => {
+      console.log(allSpikemmon);
+      createCard(allSpikemmon);
+      chargement.style.display = "none";
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 // // // Création des cartes
 
-// function createCard(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     const carte = document.createElement("li");
+function createCard(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const carte = document.createElement("li");
 
-//     let backgroundColor = arr[i].type.toLowerCase();
-//     console.log("coucou");
-//     carte.style.backgroundImage = `url(images/background_${backgroundColor}.png)`;
-//     const txtCarte = document.createElement("h5");
-//     txtCarte.innerText = arr[i].name;
-//     const stars = document.createElement("p");
-//     stars.innerHTML = displayStars(arr[i].rarity);
-//     const imgCarte = document.createElement("img");
-//     imgCarte.src = arr[i].imageUrl;
+    let backgroundColor = arr[i].type.toLowerCase();
+    console.log("coucou");
+    carte.style.backgroundImage = `url(images/background_${backgroundColor}.png)`;
+    const txtCarte = document.createElement("h5");
+    txtCarte.innerText = arr[i].name;
+    const stars = document.createElement("p");
+    stars.innerHTML = displayStars(arr[i].rarity);
+    const imgCarte = document.createElement("img");
+    imgCarte.src = arr[i].imageUrl;
 
-//     carte.appendChild(imgCarte);
-//     carte.appendChild(txtCarte);
-//     carte.appendChild(stars);
-//     listePoke.appendChild(carte);
-//   }
-// }
+    carte.appendChild(imgCarte);
+    carte.appendChild(txtCarte);
+    carte.appendChild(stars);
+    listePoke.appendChild(carte);
+  }
+}
 
 // // Recherche
-// searchInput.addEventListener("keyup", recherche);
-// function recherche() {
-//   if (index < 300) {
-//     addPoke(280);
-//   }
-//   let filter, allLi, titleValue, allTitles;
-//   filter = searchInput.value.toUpperCase();
-//   allLi = document.querySelectorAll("li");
-//   allTitles = document.querySelectorAll("li > h5");
+searchInput.addEventListener("keyup", recherche);
+function recherche() {
+  if (index < 300) {
+    addPoke(280);
+  }
+  let filter, allLi, titleValue, allTitles;
+  filter = searchInput.value.toUpperCase();
+  allLi = document.querySelectorAll("li");
+  allTitles = document.querySelectorAll("li > h5");
 
-//   for (i = 0; i < allLi.length; i++) {
-//     titleValue = allTitles[i].innerText;
+  for (i = 0; i < allLi.length; i++) {
+    titleValue = allTitles[i].innerText;
 
-//     if (titleValue.toUpperCase().indexOf(filter) > -1) {
-//       allLi[i].style.display = "flex";
-//     } else {
-//       allLi[i].style.display = "none";
-//     }
-//   }
-// }
+    if (titleValue.toUpperCase().indexOf(filter) > -1) {
+      allLi[i].style.display = "flex";
+    } else {
+      allLi[i].style.display = "none";
+    }
+  }
+}
 
 // // animation bouton de recherche
-// searchInput.addEventListener("input", function (e) {
-//   if (e.target.value !== "") {
-//     e.target.parentNode.classList.add("active-input");
-//   } else if (e.target.value === "") {
-//     e.target.parentNode.classList.remove("active-input");
-//   }
-// });
+searchInput.addEventListener("input", function (e) {
+  if (e.target.value !== "") {
+    e.target.parentNode.classList.add("active-input");
+  } else if (e.target.value === "") {
+    e.target.parentNode.classList.remove("active-input");
+  }
+});
 
-// const displayStars = (rarity) => {
-//   let result = "";
-//   for (let i = 0; i < rarity; i++) {
-//     result += '<i class="fa-solid fa-star"></i>';
-//   }
-//   console.log(result);
-//   return result;
-// };
+const displayStars = (rarity) => {
+  let result = "";
+  for (let i = 0; i < rarity; i++) {
+    result += '<i class="fa-solid fa-star"></i>';
+  }
+  console.log(result);
+  return result;
+};
